13af9902a036f019354a5cb0e23ec0286c194501
sisgestion563 <sys@tantahouse-pe.com>
2026-06-29 18:30:35 -0500

    Update auth.service.js

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,3 +1,42 @@
+const bcrypt = require('bcrypt');
+
+const authRepository =
+require('../repositories/auth.repository');
+
+const {
+    generateToken
+} = require('../utils/jwt.util');
+
+const login = async (
+    username,
+    password
+) => {
+
+    console.log(
+        'PASO 1 - BUSCANDO USUARIO',
+        username
+    );
+
+    const user =
+        await authRepository.findByUsername(
+            username
+        );
+
+    console.log(
+        'PASO 2 - USUARIO ENCONTRADO',
+        user
+    );
+
+    if (!user) {
+        throw new Error(
+            'Usuario no existe'
+        );
+    }
+
+    console.log(
+        'PASO 3 - VALIDANDO PASSWORD'
+    );
+
 const validPassword =
     await bcrypt.compare(
         password,
@@ -32,4 +71,39 @@ return {
         proveedor_id: user.proveedor_id,
         primer_ingreso: user.primer_ingreso
     }
+};
+   
+
+
+
+
+    const token =
+        generateToken({
+            usuario_id: user.usuario_id,
+            username: user.username,
+            rol: user.rol,
+            proveedor_id:
+                user.proveedor_id
+        });
+
+    return {
+        token,
+        usuario: {
+            usuario_id:
+                user.usuario_id,
+            username:
+                user.username,
+            rol:
+                user.rol,
+            proveedor_id:
+                user.proveedor_id,
+            primer_ingreso:
+                user.primer_ingreso
+        }
+    };
+
+};
+
+module.exports = {
+    login
 };
