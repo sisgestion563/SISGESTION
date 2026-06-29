54d778c622afacf6199082aed30ef864cc4e2365
sisgestion563 <sys@tantahouse-pe.com>
2026-06-29 18:33:25 -0500

    Update auth.service.js

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -54,29 +54,6 @@ if (!validPassword) {
     );
 }
 
-const token =
-    generateToken({
-        usuario_id: user.usuario_id,
-        username: user.username,
-        rol: user.rol,
-        proveedor_id: user.proveedor_id
-    });
-
-return {
-    token,
-    usuario: {
-        usuario_id: user.usuario_id,
-        username: user.username,
-        rol: user.rol,
-        proveedor_id: user.proveedor_id,
-        primer_ingreso: user.primer_ingreso
-    }
-};
-   
-
-
-
-
     const token =
         generateToken({
             usuario_id: user.usuario_id,
