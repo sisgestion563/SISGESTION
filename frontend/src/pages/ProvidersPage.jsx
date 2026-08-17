02aeee7e4b1b6de76a522877d087d6094eacdc6f
sisgestion563 <sys@tantahouse-pe.com>
2026-08-16 20:25:18 -0500

    Update ProvidersPage.jsx

--- frontend/src/pages/ProvidersPage.jsx
+++ frontend/src/pages/ProvidersPage.jsx
@@ -1398,7 +1398,7 @@ export default function ProvidersPage() {
                                 </button>
                             </div>
                         </div>
-                    </>
+                    </div>
 
             ) : (
                 /* ════════════════════════════════════════════════════════════
